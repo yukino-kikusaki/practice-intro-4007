aee6a6937758fe4293f68ce7491bb4a6ab786665
TrpFrog <dev@trpfrog.net>
2024-03-30 14:42:06 +0900

    use hono

---
+++ src/app/entry.js
@@ -0,0 +1,9 @@
+"use strict";
+import $ from "jquery";
+const block = $("#block");
+const scalingButton = $("#scaling-button");
+
+scalingButton.on("click", () => {
+  block.animate({ width: "200pt", height: "200pt" }, 2000);
+  block.animate({ width: "100pt", height: "100pt" }, 2000);
+});
